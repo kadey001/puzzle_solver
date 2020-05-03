d1c61aceb9a519cb8acebff61237a4c7dd03a4cc
kadey001 <kadey001@ucr.edu>
2020-05-02 20:37:01 -0700

    Finished blank space swapping system Moved State Class to sepearte File

--- main.py
+++ main.py
@@ -6,106 +6,24 @@ Eight-Puzzle Using:
 3. A* with the Eucledian Distance Heuristic
 """
 
-import sys
+import sys, queue
 
-def uniform_cost_search(state):
-   misplaced_dict = state.misplaced_dict
-   costs_dict = state.cost_dict
-   print(costs_dict, misplaced_dict)
+from state import State
 
-def get_position_dict(n = 3):
-   """Creates a dict for each value's correct position"""
-   position_dict = {}
-   value = 1
-   for i in range(n):
-      for j in range(n):
-         if i * j >= n**2:
-            position_dict[0] = (i, j)
-            return position_dict
-         else:
-            position_dict[value] = (i, j)
-            value += 1
-   return position_dict
+def uniform_cost_search(initial_state):
+   misplaced_dict = initial_state.misplaced_dict
+   costs_dict = initial_state.cost_dict
+   #print(costs_dict, misplaced_dict)
+   #print(initial_state.get_heuristic())
 
-class Node:
-   def __init__(self, value = None, left = None, right = None):
-      self.value = value
-      self.left = left
-      self.right = right
+   q = queue.Queue()
 
-class Tree:
-   def __init__(self, root):
-      self.root = root
+   swaps = initial_state.get_swaps()
+   for item in swaps:
+      item.print_state()
 
-   def insert(self):
-      pass
-
-class State:
-   def __init__(self, initial_state, n = 3):
-      self.initial_state = initial_state
-      self.current_state = initial_state
-      self.history = []
-      self.n = n
-      self.set_misplaced_dict()
-      self.set_cost_dict()
-
-   def print_state(self):
-      """Prints out the current state of the puzzle"""
-      for row in self.current_state:
-         row_str = '|'
-         for item in row:
-            row_str += (str(item) + '|')
-         print(row_str)
-      print('')
-
-   def set_misplaced_dict(self):
-      """
-      Sets up a dictionay with tuples that gives all of the 
-      positions for each misplaced square in the puzzle {key=value, (row, colum)}
-      """
-      misplaced_dict = {}
-      expected_value = 1
-      #go in order from 1 to end to find the values that are misplaced
-      for row, row_item in enumerate(self.current_state):
-         for colum, value in enumerate(row_item):
-            if int(value) is 0:
-               #ignore empty tile
-               pass
-            elif int(value) is not expected_value:
-               misplaced_dict[int(value)] = (row, colum)
-            
-            expected_value += 1
-      
-      self.misplaced_dict = misplaced_dict
-
-   def set_cost_dict(self):
-      """
-      Finds all the costs for each misplaced square and adds them to a cost dictionary
-      with the keys being the square values {key=value, cost}
-      """
-      position_dict = get_position_dict(self.n)
-      cost_dict = {}
-      for key in self.misplaced_dict:
-         #(row, colum, value) of a misplaced element
-         misplaced_item_position = self.misplaced_dict[key]
-
-         misplaced_row = misplaced_item_position[0]
-         misplaced_colum = misplaced_item_position[1]
-
-         expected_item_position = position_dict[key]
-         correct_row = expected_item_position[0]
-         correct_colum = expected_item_position[1]
-
-         #Find the cost for moving to correct position and add it to the cost_dict
-         cost = abs(misplaced_row - correct_row) + abs(misplaced_colum - correct_colum)
-         cost_dict[key] = cost
-      
-      self.cost_dict = cost_dict
-
-   def update_state(self, updated_state, action):
-      """Updates the current state of the puzzle"""
-      self.current_state = updated_state
-      self.history.append(action)
+   #root = Node(initial_state)
+   #Add all children for current frontier
 
 def create_custom_puzzle():
    """
@@ -134,9 +52,9 @@ def create_default_puzzle():
    Creates a default starting puzzle
    """
    default = [
-      ['1', '2', '3'],
-      ['4', '5', '6'],
-      ['8', '7', '0']
+      [1, 2, 3],
+      [4, 5, 6],
+      [8, 0, 7]
    ]
 
    return default
